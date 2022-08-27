20acd5ab2783b6c04d6d85fe6a7d6da463402a4c
max <maksim.kalganov@gmail.com>
2022-08-27 13:16:07 +0300

    fix exp bugs

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -8,10 +8,17 @@ from src.utils import dump_results, get_single_experiment_results
 
 
 @gin.configurable()
-def run_experiment(batch_size: int, epochs: int, tensorboard_logs_name: str, results_file: str, exp_name: str):
+def run_experiment(
+        batch_size: int,
+        epochs: int,
+        tensorboard_logs_name: str,
+        results_file: str,
+        exp_name: str,
+        exp_comments: str
+) -> None:
     tensorboard_logs = os.path.join(TENSORBOARD_LOGS_DIR, tensorboard_logs_name)
     exp_results = get_single_experiment_results(batch_size, epochs, tensorboard_logs, seed=0)
-    dump_results(exp_results, results_file, exp_name)
+    dump_results(exp_results, results_file, exp_name, exp_comments)
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 """Utils"""
 import os
+from typing import List
 
 from src.dataset_generator import get_dataset
 from src.model import get_model
 import keras
 import pandas as pd
+from src import constants as ct
 
 
 def get_single_experiment_results(
@@ -12,7 +14,7 @@ def get_single_experiment_results(
         epochs: int,
         tensorboard_logs: str,
         seed: int
-) -> None:
+) -> List[float]:
     x_train, y_train, x_test, y_test = get_dataset(seed=seed)
     model = get_model(seed=seed)
 
@@ -28,10 +30,17 @@ def get_single_experiment_results(
     return model.evaluate(x_test, y_test)
 
 
-def dump_results(model_results, results_file: str, exp_name: str):
-    os.makedirs(os.path.dirname(results_file))
+def dump_results(model_results: List[float], results_file: str, exp_name: str, exp_comments: str):
+    os.makedirs(os.path.dirname(results_file), exist_ok=True)
 
-    df = pd.read_csv(results_file, index=False) if os.path.exists(results_file) else pd.DataFrame()
+    df = pd.read_csv(results_file) if os.path.exists(results_file) else pd.DataFrame()
 
-    new_record = pd.Series({""})
+    new_record = pd.Series({
+        ct.EXPERIMENT_NAME: exp_name,
+        ct.EXPERIMENT_LOSS: model_results[0],
+        ct.EXPERIMENT_ACCURACY: model_results[1],
+        ct.EXPERIMENT_COMMENTS: exp_comments
+    })
 
+    df = df.append(new_record, ignore_index=True)
+    df.to_csv(results_file, index=False)
