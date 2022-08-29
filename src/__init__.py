f43e966e015ef403b1bcfcb7a492009564cf39c7
max <maksim.kalganov@gmail.com>
2022-08-29 10:57:40 +0300

    add caller for the regular dropout and experts dropout

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@
 
 from .dataset_generator import get_dataset
 from .model import get_model
+from .dropout_exp import regular_dropout, experts_dropout
--- src/dropout_exp.py
+++ src/dropout_exp.py
@@ -7,11 +7,6 @@ import tensorflow as tf
 from src.constants import COMMON_DTYPE
 
 
-@gin.configurable
-def regular_dropout(x_input, prob):
-    return tf.keras.layers.Dropout(prob)(x_input)
-
-
 class ExpertsDropout(tf.keras.layers.Layer):
     def __init__(self, features_scores: List[float]):
         super().__init__()
@@ -26,3 +21,13 @@ class ExpertsDropout(tf.keras.layers.Layer):
             prob = tf.random.uniform(shape=inputs.shape, minval=0, maxval=1, dtype=COMMON_DTYPE)
             result = tf.where(prob < self.features_scores, inputs, 0.)
         return result
+
+
+@gin.configurable()
+def regular_dropout(x_input, prob):
+    return tf.keras.layers.Dropout(prob)(x_input)
+
+
+@gin.configurable()
+def experts_dropout(x_input, features_scores):
+    return ExpertsDropout(features_scores)(x_input)
