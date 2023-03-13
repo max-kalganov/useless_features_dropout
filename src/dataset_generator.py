1f8b73f13d5e66193e2e02ead2ccedfa5af4757a
max <maksim.kalganov@gmail.com>
2023-03-13 21:29:53 +0300

    implement simple mnist loader

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -5,6 +5,8 @@ import gin
 import numpy as np
 
 from src.constants import FEATURES_NUMBER
+import tensorflow_datasets as tfds
+import tensorflow as tf
 
 
 @gin.configurable()
@@ -19,3 +21,37 @@ def get_dataset(
 
     split = int(n_samples * train_test_split)
     return x[:split, :], y[:split], x[split:, :], y[split:]
+
+
+def get_mnist_dataset() -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+    def normalize_img(image, label):
+        """Normalizes images: `uint8` -> `float32`."""
+        return tf.cast(image, tf.float32) / 255., label
+
+    def reshape_img(image, label):
+        """Flatten image"""
+        return tf.reshape(image, (-1,)), label
+
+    def process_dataset(dataset, is_train: bool):
+        dataset = dataset.map(
+            normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+        dataset = dataset.map(
+            reshape_img, num_parallel_calls=tf.data.AUTOTUNE)
+        dataset = dataset.cache()
+        if is_train:
+            dataset = dataset.shuffle(ds_info.splits['train'].num_examples)
+        dataset = dataset.batch(128)
+        dataset = dataset.prefetch(tf.data.AUTOTUNE)
+        return dataset
+
+    (ds_train, ds_test), ds_info = tfds.load(
+        'mnist',
+        split=['train', 'test'],
+        shuffle_files=True,
+        as_supervised=True,
+        with_info=True,
+    )
+
+    ds_train = process_dataset(ds_train, is_train=True)
+    ds_test = process_dataset(ds_test, is_train=False)
+    return ds_train, ds_test
