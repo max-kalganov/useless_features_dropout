42b0f3ba95d17407b12a911830ead68933bf9193
max <maksim.kalganov@gmail.com>
2023-03-13 22:14:48 +0300

    extend __init__ with gin.configurable functions

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 """Imports for correct gin config working"""
 
-from .dataset_generator import get_dataset
-from .model import get_model
-from .dropout_exp import regular_dropout, experts_dropout
+from .dataset_generator import get_dataset, get_mnist_dataset
+from .model import get_model, get_model_mnist
+from .dropout_exp import regular_dropout, experts_dropout, permutation_fi_dropout, get_feature_importance
