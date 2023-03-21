1a102ac0f9a9f3f7c5fc9b16aea07f5d9ad616a6
max <maksim.kalganov@gmail.com>
2023-03-21 07:58:01 +0300

    implement simple script for dumping feature importance

---
+++ scripts/run_permutation_importance.py
@@ -0,0 +1,17 @@
+import gin
+
+from src import get_mnist_dataset
+from src.features_importance import permutation_fi_scores
+
+
+@gin.configurable
+def dump_features_importance(features_importance_file: str):
+    ds_train, _ = get_mnist_dataset()
+    features_importance = permutation_fi_scores(ds_train=ds_train)
+    features_importance.save(features_importance_file)
+    print(f"Features importance {features_importance} are saved into {features_importance_file}")
+
+
+if __name__ == '__main__':
+    gin.parse_config_file("configs/mnist_perm_feature_importance_exp.gin")
+    dump_features_importance()
