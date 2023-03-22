f7ca1b90def3132bec911a8afe38273305c7bdc5
max <maksim.kalganov@gmail.com>
2023-03-22 14:40:18 +0300

    correct experiment script for heart attack dataset

--- scripts/run_experiment_feature_importance.py
+++ scripts/run_experiment_feature_importance.py
@@ -3,9 +3,10 @@ import gin
 from src.experiments_runner import ExperimentsRunner
 import tensorflow as tf
 gin.external_configurable(tf.keras.losses.CategoricalCrossentropy)
+gin.external_configurable(tf.keras.losses.BinaryCrossentropy)
 
 
 if __name__ == '__main__':
-    gin.parse_config_file("configs/mnist_perm_feature_importance_exp.gin")
+    gin.parse_config_file("configs/heart_attack_perm_feature_importance_exp.gin")
     exp_runner = ExperimentsRunner()
     exp_runner.run_experiment()
