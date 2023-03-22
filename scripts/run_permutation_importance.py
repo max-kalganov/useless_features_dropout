63efd1313de696409935390786f94e5cdf95a68d
max <maksim.kalganov@gmail.com>
2023-03-22 22:31:56 +0300

    move dump_features_importance into utils

--- scripts/run_permutation_importance.py
+++ scripts/run_permutation_importance.py
@@ -1,25 +1,6 @@
-import os
-
 import gin
-import numpy as np
 
-from src import get_heart_attack_dataset
-from src.features_importance import permutation_fi_scores
-from src.utils import get_feature_importance_files
-
-
-@gin.configurable
-def dump_features_importance(features_importance_dir: str):
-    features_importance_file, args_to_leave_file = get_feature_importance_files(features_importance_dir)
-
-    ds_train, _ = get_heart_attack_dataset()
-    features_importance, args_to_leave = permutation_fi_scores(ds_train=ds_train)
-    os.makedirs(os.path.dirname(features_importance_file), exist_ok=True)
-    np.save(features_importance_file, features_importance)
-    np.save(args_to_leave_file, args_to_leave)
-    print(f"Features importance {features_importance} are saved into {features_importance_file}")
-    print(f"Args to leave {args_to_leave} are saved into {args_to_leave_file}")
-
+from src.utils import dump_features_importance
 
 if __name__ == '__main__':
     gin.parse_config_file("configs/features_importance_config.gin")
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,26 @@
 import os
 from typing import Tuple
 
+import gin
+import numpy as np
+
 
 def get_feature_importance_files(features_importance_dir: str) -> Tuple[str, str]:
-    feature_importance_file = os.path.join(features_importance_dir, 'feature_importance.np')
-    args_to_leave_file = os.path.join(features_importance_dir, 'args_to_leave.np')
+    feature_importance_file = os.path.join(features_importance_dir, 'feature_importance.npy')
+    args_to_leave_file = os.path.join(features_importance_dir, 'args_to_leave.npy')
     return feature_importance_file, args_to_leave_file
+
+
+@gin.configurable
+def dump_features_importance(features_importance_dir: str):
+    from src import get_heart_attack_dataset, permutation_fi_scores
+
+    features_importance_file, args_to_leave_file = get_feature_importance_files(features_importance_dir)
+
+    ds_train, _ = get_heart_attack_dataset()
+    features_importance, args_to_leave = permutation_fi_scores(ds_train=ds_train)
+    os.makedirs(os.path.dirname(features_importance_file), exist_ok=True)
+    np.save(features_importance_file, features_importance)
+    np.save(args_to_leave_file, args_to_leave)
+    print(f"Features importance {features_importance} are saved into {features_importance_file}")
+    print(f"Args to leave {args_to_leave} are saved into {args_to_leave_file}")
