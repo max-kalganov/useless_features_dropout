3da25c63756847c7f647c8d74e4cd49b8f601b73
max <maksim.kalganov@gmail.com>
2023-03-22 22:33:10 +0300

    correct experts drop out

--- src/dropout_exp.py
+++ src/dropout_exp.py
@@ -1,4 +1,5 @@
 """Implementations of custom and regular dropouts to be used on model"""
+import sys
 from typing import List, Union
 
 import gin
@@ -6,6 +7,7 @@ import numpy as np
 import tensorflow as tf
 
 from src.constants import COMMON_DTYPE
+from src.utils import get_feature_importance_files
 
 
 class ExpertsDropout(tf.keras.layers.Layer):
@@ -20,14 +22,16 @@ class ExpertsDropout(tf.keras.layers.Layer):
     def __init__(self, features_scores: Union[List[float], np.ndarray]):
         super().__init__()
         assert all(0 <= imp <= 1 for imp in features_scores), "incorrect feature importance is set"
-        assert isinstance(features_scores, list), "incorrect features_scores type"
+        assert isinstance(features_scores, list) or isinstance(features_scores, np.ndarray),\
+            "incorrect features_scores type"
         self.features_scores = features_scores
 
+    @tf.function
     def call(self, inputs, training=False):
         result = inputs
-        if training and inputs.shape[0] is not None:
-            prob = tf.random.uniform(shape=inputs.shape, minval=0, maxval=1, dtype=COMMON_DTYPE)
-            result = tf.where(self.features_scores <= prob, inputs, 0.)
+        if training:
+            prob = tf.random.uniform(shape=(inputs.shape[1], ), minval=0, maxval=1, dtype=COMMON_DTYPE)
+            result = tf.where(self.features_scores * 10 <= prob, inputs, 0.)
         return result
 
 
@@ -44,6 +48,7 @@ def experts_dropout(x_input, features_scores):
 
 
 @gin.configurable
-def feature_importance_dropout(x_input, features_importance_file: str) -> ExpertsDropout:
+def feature_importance_dropout(x_input, features_importance_dir: str) -> ExpertsDropout:
+    features_importance_file, _ = get_feature_importance_files(features_importance_dir)
     features_importance = np.load(features_importance_file)
     return ExpertsDropout(features_importance)(x_input)
